6cf715001b943a90a5e5a1ecba720a22e5c767b1
rkeerthient <rkeerthi@yext.com>
2024-11-08 00:15:57 +0530

    bjc-hc

---
+++ src/components/cards/ProfessionalStandard.tsx
@@ -0,0 +1,140 @@
+import { MapPinIcon, PhoneIcon } from "@heroicons/react/20/solid";
+import { BsCheckCircle } from "react-icons/bs";
+import {
+  Address,
+  AddressType,
+  getDirections,
+  HoursStatus,
+  Image,
+} from "@yext/pages-components";
+import { CardProps } from "@yext/search-ui-react";
+import Cta from "../cta";
+import { format_phone } from "../../utils/reusableFunctions";
+import ResponseComponent from "../ResponseComponent";
+
+const ProfessionalStandard = ({ result }: CardProps<any>) => {
+  const { name, index, distance } = result;
+  const {
+    id,
+    description,
+    headshot,
+    mainPhone,
+    hours,
+    landingPageUrl,
+    address,
+    timezone,
+    c_primaryCTA,
+    c_secondaryCTA,
+    slug,
+    c_professional,
+    taxonomy_relatedSpecialties,
+    acceptingNewPatients,
+  } = result.rawData;
+
+  return (
+    <article className="border rounded-xl shadow-md flex justify-between  pr-4 text-[#212529] bg-slate-100">
+      <section className="px-2 space-y-1 ml-4 flex-1 mt-4">
+        <header className="flex justify-between">
+          <a
+            id={`location-${id}`}
+            target="_blank"
+            href={slug}
+            className="font-semibold text-orange flex items-center space-x-2 standardTitle"
+            rel="noreferrer"
+          >
+            <h2 className="text-xl md:text-3xl">{name}</h2>
+          </a>
+        </header>
+
+        <section className="flex flex-col justify-between  gap-8 pb-2">
+          <div>
+            <div className="flex items-start md:justify-start gap-8 py-4 flex-col md:flex-row">
+              <header className="relative flex flex-col md:w-1/6 w-full">
+                <a
+                  href={landingPageUrl}
+                  className=" group aspect-square block   overflow-hidden rounded-t-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100"
+                >
+                  {headshot && (
+                    <Image
+                      image={headshot!}
+                      className="pointer-events-none object-cover group-hover:opacity-75 !h-full !w-full !max-w-none !object-top"
+                    />
+                  )}
+                </a>
+              </header>
+              <div>
+                {hours ? (
+                  <HoursStatus
+                    timezone={timezone}
+                    hours={hours}
+                    className="md:text-lg font-medium"
+                  />
+                ) : (
+                  <p></p>
+                )}
+                {taxonomy_relatedSpecialties && (
+                  <div className="flex gap-2 my-4 ">
+                    {taxonomy_relatedSpecialties.map(
+                      (item: any, index: number) => (
+                        <div
+                          className="px-4 py-1 text-sm rounded-full bg-white border border-[#212529]"
+                          key={index}
+                        >
+                          {item.name}
+                        </div>
+                      )
+                    )}
+                  </div>
+                )}
+                {address && (
+                  <address className="flex  justify-start font-medium leading-loose items-start   text-secondary not-italic">
+                    <MapPinIcon className="h-4 w-4 mt-2" />
+                    <span className="ml-2">
+                      {address.line1}, {address.city}, {address.region} -{" "}
+                      {address.postalCode}
+                    </span>{" "}
+                    <a className="ml-2" href={getDirections(address)}>
+                      (See Map)
+                    </a>
+                  </address>
+                )}
+                {mainPhone && (
+                  <section className="flex  justify-start font-medium leading-loose items-center   text-secondary">
+                    <PhoneIcon className="h-4 w-4" />
+                    <span className="ml-2">
+                      <a href={`tel:${mainPhone}`}>{format_phone(mainPhone)}</a>
+                    </span>
+                  </section>
+                )}
+                {acceptingNewPatients && (
+                  <span className="flex gap-2 items-center px-4 py-1 rounded-full bg-green-100 w-fit">
+                    <BsCheckCircle className="h-5 w-5" /> Accepting New Patients{" "}
+                  </span>
+                )}
+              </div>
+              <footer className="hidden md:flex  flex-col gap-2 justify-center pt-4 pb-2 items-center uppercase mb-auto ml-auto">
+                {c_primaryCTA && (
+                  <Cta cta={c_primaryCTA} ctaType="primaryCta" />
+                )}
+                {c_secondaryCTA && (
+                  <Cta cta={c_secondaryCTA} ctaType="secondaryCta" />
+                )}
+              </footer>
+            </div>
+            {description && (
+              <ResponseComponent response={description} showMore={true} />
+            )}
+            <footer className="flex flex-col md:hidden  gap-2 justify-center pt-4 pb-2 items-center uppercase mb-auto">
+              {c_primaryCTA && <Cta cta={c_primaryCTA} ctaType="primaryCta" />}
+              {c_secondaryCTA && (
+                <Cta cta={c_secondaryCTA} ctaType="secondaryCta" />
+              )}
+            </footer>
+          </div>
+        </section>
+      </section>
+    </article>
+  );
+};
+
+export default ProfessionalStandard;
---
+++ src/config/VerticalConfig.ts
@@ -0,0 +1,161 @@
+import {
+  CardComponent,
+  DefaultRawDataType,
+  VerticalConfigMap,
+} from "@yext/search-ui-react";
+
+import { UniversalSection } from "../components/search/UniversalSection";
+import ProductProminentVideo from "../components/cards/ProductProminentVideo";
+import FileStandard from "../components/cards/FileStandard";
+import EventStandard from "../components/cards/EventStandard";
+import FAQAccordion from "../components/cards/FAQAccordion";
+import JobStandard from "../components/cards/JobStandard";
+import LocationStandard from "../components/cards/LocationStandard";
+import ProfessionalLocationAndGrid from "../components/cards/ProfessionalLocationAndGrid";
+import ProfessionalStandard from "../components/cards/ProfessionalStandard";
+import ProductProminentImage from "../components/cards/ProductProminentImage";
+
+export interface GlobalConfigProps {
+  accountEnv: "Production" | "Sandbox";
+  searchExperienceVersion: "Production" | "Staging";
+  chatExperienceVersion: "Production" | "Staging";
+  isChatEnabled: boolean;
+  locale: string;
+  isGenerativeDirectAnswerEnabled: boolean;
+  region: "US" | "EU";
+}
+
+export const GlobalConfig: GlobalConfigProps = {
+  accountEnv: "Production", //Production or Sandbox
+  searchExperienceVersion: "Production", // Production or Staging
+  chatExperienceVersion: "Production", // Production or Staging
+  isChatEnabled: false, //true or false
+  locale: "en", // your locale eg: en_GB
+  isGenerativeDirectAnswerEnabled: false, //true or false
+  region: "US", //US or EU
+};
+/**
+ * 
+ * Define properties for each section type shown in the search interface.
+ * label: Display name for the section.
+ * verticalKey: Optional key that uniquely identifies each type of content (e.g., 'faq', 'jobs').
+ * pageType: Defines layout style, e.g., "grid-cols-2" (two-column grid), "map" (map view).
+ * universalLimit: Limits the number of items shown in universal search.
+ * sortByOptions: Custom sorting options for items.
+ * cardType: Specifies how each item in the section is displayed (using different card components).
+ * visualTypeHead: Boolean to enable special header visuals (optional).
+ * 
+ * 
+ * Recommendation 
+ * gridPageType - ProductProminentVideo, LocationStandard, ProfessionalLocationAndGrid, ProductProminentImage
+ * 
+ * 
+ * Sample sorting format
+ * 
+ sortByOptions: [
+  {
+    label: "Name: A-Z",
+    sortBy: {
+      field: "name",
+      direction: Direction.Ascending,
+      type: SortType.Field,
+    },
+  },
+  {
+    label: "Name: Z-A",
+    sortBy: {
+      field: "name",
+      direction: Direction.Descending,
+      type: SortType.Field,
+    },
+  },
+],
+ * 
+ */
+
+export interface VerticalProps {
+  label: string;
+  verticalKey?: string;
+  pageType:
+    | "grid-cols-2"
+    | "grid-cols-3"
+    | "grid-cols-4"
+    | "standard"
+    | "map"
+    | "universal";
+  universalLimit?: number;
+  verticalLimit?: number;
+  sortFields?: string[];
+  cardType?: CardComponent;
+  visualTypeHead?: boolean;
+}
+
+export const VerticalConfig: VerticalProps[] = [
+  {
+    label: "All",
+    pageType: "universal",
+  },
+  // {
+  //   label: "FAQs",
+  //   verticalKey: "faq",
+  //   pageType: "standard",
+  //   cardType: FAQAccordion,
+  //   universalLimit: 3,
+  //   verticalLimit: 5,
+  // },
+  {
+    label: "Professionals",
+    verticalKey: "healthcare-professional",
+    pageType: "standard",
+    cardType: ProfessionalStandard,
+    universalLimit: 3,
+  },
+  {
+    label: "Locations",
+    verticalKey: "healthcare-facilities",
+    pageType: "map",
+    cardType: LocationStandard,
+    universalLimit: 3,
+  },
+  // {
+  //   label: "Jobs",
+  //   verticalKey: "jobs",
+  //   pageType: "standard",
+  //   cardType: JobStandard,
+  //   universalLimit: 3,
+  //   verticalLimit: 5,
+  // },
+  // {
+  //   label: "Events",
+  //   verticalKey: "events",
+  //   pageType: "standard",
+  //   cardType: EventStandard,
+  //   universalLimit: 3,
+  //   verticalLimit: 5,
+  // },
+  {
+    label: "Products",
+    verticalKey: "services",
+    pageType: "grid-cols-3",
+    cardType: ProductProminentImage,
+    universalLimit: 3,
+  },
+];
+
+// Configuration options for enabling or disabling features
+
+export const UniversalConfig: VerticalConfigMap<
+  Record<string, DefaultRawDataType>
+> = VerticalConfig.reduce(
+  (configMap, item) => {
+    if (item.verticalKey) {
+      configMap[item.verticalKey] = {
+        CardComponent: item.cardType,
+        SectionComponent: UniversalSection,
+        label: item.label,
+      };
+    }
+    return configMap;
+  },
+  {} as VerticalConfigMap<Record<string, DefaultRawDataType>>
+);
